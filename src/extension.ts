5c149e7418513e761aaf6cc757f58db6ea7aa91e
Cyril Lamotte <clamotte@jouve.com>
2023-10-26 14:24:39 +0200

    First commit

---
+++ src/extension.ts
@@ -0,0 +1,10 @@
+// The module 'vscode' contains the VS Code extensibility API
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
